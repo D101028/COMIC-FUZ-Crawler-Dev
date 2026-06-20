765e775fad7cbb47ce18bd37a605e57bf8e1f6c7
LKK <Damont101028@gmail.com>
2026-06-20 20:44:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  fullizePos:     {  x:478,  y:773 },
-  normalPos:      {  x:1078, y:429 },
-  nextPagePos:    {  x:50,   y:50  }
+  fullizePos:     {  x:950,  y:1237 },
+  normalPos:      {  x:1627, y:750 },
+  nextPagePos:    {  x:100,   y:100  }
 };
