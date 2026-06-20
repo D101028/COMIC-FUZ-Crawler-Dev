de62700bd94d8f5a4188f446dd33b6056dc8fbe8
LKK <Damont101028@gmail.com>
2026-06-20 20:58:40 +0800

    Update waiting time

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const CONFIG = {
   fullizePos:     {  x:950,  y:1237 },
   normalPos:      {  x:1627, y:750 },
-  nextPagePos:    {  x:100,   y:100  }
+  nextPagePos:    {  x:100,   y:100  },
+  loadingDelayMs: 3900
 };
